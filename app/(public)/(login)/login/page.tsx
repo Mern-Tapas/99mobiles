0453a88b3dbb5769a506d54ed330efa6214e2ebc
Mern-Tapas <cyb6261452510@gmail.com>
2023-12-25 16:04:27 +0530

    changes

--- app/(public)/(login)/login/page.tsx
+++ app/(public)/(login)/login/page.tsx
@@ -1,20 +1,6 @@
-'use client'
 import React from 'react'
-import { getProviders } from 'next-auth/react'
-import { useState } from 'react'
 
 function page() {
-
-    const [provider, setprovider] = useState([{
-        clientId:"",
-        clientAuth:""
-    }])
-
-    const getproviders = async () => {
-        const providers = await getProviders()
-        setprovider(providers)
-    }
-    getproviders()
 
     return (
         <div></div>
