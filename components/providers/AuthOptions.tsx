3093ef5203cf0751f790393fcba46138e0609da3
Mern-Tapas <cyb6261452510@gmail.com>
2024-01-02 09:07:37 +0530

    update login page

--- components/providers/AuthOptions.tsx
+++ components/providers/AuthOptions.tsx
@@ -3,6 +3,7 @@ import { signIn, signOut } from 'next-auth/react'
 import { useSession } from 'next-auth/react'
 import DivPrimary from '../DivPrimary'
 import GoogleIcon from '@/public/icons/google.svg'
+import Link from 'next/link'
 
 function AuthOptions({ className }: { className: string }) {
 
@@ -11,7 +12,7 @@ function AuthOptions({ className }: { className: string }) {
 
 
   return (
-    <DivPrimary className={`${className} transition-all	 ${session ? "" : ""} h-[550px] lg:w-[400px] w-[350px] rounded `}>
+    <DivPrimary className={`${className} transition-all	 ${session ? "" : ""}  lg:w-[400px] w-[350px] rounded `}>
       <div className='h-full w-full p-6'>
 
         <div className='mb-4'>
@@ -19,7 +20,7 @@ function AuthOptions({ className }: { className: string }) {
           <p className='text-center text-slate-400'>Hey, Enter Your details to get sing in to your account</p>
         </div>
 
-        <div>
+        <div className='mb-4'>
           <button className='border flex w-full p-3 rounded relative' onClick={() => { signIn("google") }}>
             <div className='h-[25px] w-[25px] me-auto'>
               <GoogleIcon className="h-full w-full" />
@@ -28,7 +29,28 @@ function AuthOptions({ className }: { className: string }) {
           </button>
         </div>
 
+        <div className='mb-4'>
+          <p className='text-center'>or</p>
+        </div>
 
+        <div className='mb-4'>
+          <div className='flex flex-col mb-2' >
+            <span className='text-slate-400 mb-2'>Email</span>
+            <input type="email" placeholder='Enter your email' className='border rounded w-full p-3' />
+          </div>
+          <div className='flex flex-col mb-2' >
+            <span className='text-slate-400 mb-2'>Password</span>
+            <input type="email" placeholder='Enter your password' className='border rounded w-full p-3' />
+          </div>
+        </div>
+
+        <div className='mb-4'>
+          <button className='p-3 text-center bg-black text-white rounded w-full'>Login</button>
+        </div>
+
+        <div className=''>
+          <Link href='/reset' className='text-sm text-blue-600'>Reset Your Password</Link>
+        </div>
       </div>
     </DivPrimary>
   )
