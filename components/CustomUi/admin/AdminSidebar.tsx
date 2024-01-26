e61ae88e31fadf1233fb185928206e03b17d1e11
Mern-Tapas <cyb6261452510@gmail.com>
2024-01-26 21:13:50 +0530

    sidebar udpate

--- components/CustomUi/admin/AdminSidebar.tsx
+++ components/CustomUi/admin/AdminSidebar.tsx
@@ -1,19 +1,36 @@
+'use client'
 import DivSecondary from '@/components/DivSecondary'
 import React from 'react'
-import { Button } from "@/components/ui/button"
-
+import logo from '@/public/images/company/logo.jpg'
+import Link from 'next/link'
+import Dashboard from '@/public/icons/profile.svg'
+import Image from 'next/image'
 
 function AdminSidebar() {
     return (
         <DivSecondary className='AdminSidebar  h-full w-[190px] shrink-0'>
-            <div className='border p-2 flex items-center'>
-                <div className='h-[30px] w-[30px] border me-2'>
-
+            <div className=' p-2 flex items-center'>
+                <div className='h-[40px] w-[40px] me-1'>
+                    <Image alt='logo' height={100} width={100} src={logo} />
                 </div>
                 <h2 className='font-bold'>Company Name</h2>
             </div>
-            <div className="border p-2 mt-2">
-
+            <div className=" p-2 mt-2">
+                <h2 className='text-sm font-medium text-gray-500 px-1'>Menu</h2>
+                <div className='mt-2'>
+                    <Link href='/' className='flex items-center  p-1 py-2 hover:bg-gray-100 rounded'>
+                        <span className='h-[20px] w-[20px] me-1'>
+                            <Dashboard className="h-full w-full" />
+                        </span>
+                        <p className='text-sm font-medium'>Dashboard</p>
+                    </Link>
+                    <Link href='/' className='flex items-center  p-1 py-2 hover:bg-gray-100 rounded'>
+                        <span className='h-[20px] w-[20px] me-1'>
+                            <Dashboard className="h-full w-full" />
+                        </span>
+                        <p className='text-sm '>Users</p>
+                    </Link>
+                </div>
             </div>
         </DivSecondary>
     )
