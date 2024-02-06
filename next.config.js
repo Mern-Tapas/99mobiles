ad3c53b840971d84e4b851ae1a814da4178e3d03
Mern-Tapas <cyb6261452510@gmail.com>
2024-02-06 12:48:57 +0530

    export new admincontext for error checking

--- next.config.js
+++ next.config.js
@@ -18,6 +18,9 @@ const nextConfig = {
 
         return config
     },
+    eslint: {
+        dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    },
 }
 
 module.exports = nextConfig
