4bef31ecbfc1ebe327c40fd60783e917f633d0b6
Mern-Tapas <cyb6261452510@gmail.com>
2024-02-28 10:44:05 +0530

    update

--- components/CustomUi/landingPage/UserDropDown.tsx
+++ components/CustomUi/landingPage/UserDropDown.tsx
@@ -16,22 +16,34 @@ function UserDropDown() {
     return (
         <div className='flex items-center  p-1 rounded-full'>
 
-
-            <button className="group object-cover rounded-full relative">
-                <div className="imagebox h-[35px] w-[35px] rounded-full overflow-hidden">
-                    {session?.user?.image ?
-                        <Image alt='profile image' className='h-full w-full' src={session?.user?.image || ""} height={200} width={200} />
-                        : ""
-                    }
+            <button className='ms-2 text-xs bg-white flex items-center rounded-full border group relative'>
+                <div className='h-[37px] w-[37px] p-1  rounded-full '>
+                    <div className="h-full w-full border rounded-full box overflow-hidden">
+                        {session?.user?.image ?
+                            <Image alt='profile image' className='h-full w-full' src={session?.user?.image || ""} height={200} width={200} />
+                            : ""
+                        }
+                    </div>
                 </div>
-                <div className='group-hover:opacity-100 transition-all group-hover:pointer-events-auto pointer-events-none group-hover:top-0 group opacity-0   right-0 -top-4 duration-75 z-20  absolute'>
-                    <DivPrimary className='w-[210px] p-1 rounded mt-12 shadow-lg'>
+                <div className='lg:block hidden my-auto flex flex-col'>
+                    <p className='text-start capitalize text-[10px] '>{session?.user?.name}</p>
+                    <p className='text-start capitalize text-[9px]'>{session?.user?.email}</p>
+
+                </div>
+                <div className='lg:block hidden h-[37px] w-[37px] p-1  rounded-full '>
+                    <div className="h-full w-full  rounded-full box">
+
+                    </div>
+                </div>
+
+                <div className='group-hover:opacity-100 transition-all group-hover:pointer-events-auto pointer-events-none group-hover:top-10  group opacity-0  right-0 top-9 duration-75 z-20  absolute'>
+                    <DivPrimary className='w-[210px] p-1 rounded mt-4 shadow-lg'>
                         <Link className='capitalize p-3 block w-full text-xs rounded mb-1 hover:bg-gray-50 text-left flex' href={"user"}>
                             <div className='h-[16px] w-[16px] '>
                                 <DashboardIcon className="h-full w-full" />
                             </div>
                             <span className='ms-3'>dashboard</span>
-                        </Link> 
+                        </Link>
                         <Link className='capitalize p-3 block w-full text-xs rounded mb-1 hover:bg-gray-50 text-left flex' href={"profile"}>
                             <div className='h-[16px] w-[16px] '>
                                 <ProfileIcon className="h-full w-full" />
@@ -44,16 +56,17 @@ function UserDropDown() {
                             </div>
                             <span className='ms-3'>Saved</span>
                         </Link>
-                        <button className='text-xs flex p-3 w-full rounded bg-red-600 text-white hover:bg-red-700 text-left' onClick={() => { signOut() }}>
+                        <div className='text-xs flex p-3 w-full rounded bg-red-600 text-white hover:bg-red-700 text-left' onClick={() => { signOut() }}>
                             <div className='h-[16px] w-[16px] '>
                                 <LogoutIcon className="h-full w-full" />
                             </div>
                             <span className='ms-3'>Logout</span>
-                        </button>
+                        </div>
                     </DivPrimary>
                 </div>
 
             </button>
+
         </div>
 
     )
